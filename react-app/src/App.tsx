8c625f9c7622f460ee98e1ef94417d7fe72af9ec
Mikael Kristoffersen <mikaelkristoffersen@gmail.com>
2020-03-13 12:11:01 +0100

    updated link

---
+++ react-app/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          MIT - Cloud and DevOps - Deploying React to Github Page
+        </p>
+        <a
+          className="App-link"
+          href="https://cranky-bardeen-31b610.netlify.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Assignment 1 - AWS page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
